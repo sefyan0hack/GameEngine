cb74176042d1fea6799c87e4ebe6624a183f8b0e
sefyan0hack <sefyanhack@gmail.com>
2024-07-27 12:40:10 +0100

    make window inline globla for Glcontext

--- src/G_vars.hpp
+++ src/G_vars.hpp
@@ -2,22 +2,15 @@
 #include "Global_H.hpp"
 #include <windows.h>
 #include "Renderer.hpp"
+#include "VAO.hpp"
+#include "VBO.hpp"
+#include "Shader.hpp"
+
 NO_WARNING_BEGIN
 #include <glad/glad.h>
 NO_WARNING_END
 
 #define WM_FPS_PAINT (WM_USER + 1)
-
-// OpenGl Vars
-inline GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    };
-inline GLint xL;
-inline GLint yL;
-inline GLint zL;
-inline GLfloat xyz[3] = {0.0f, 0.0f, 0.0f};
 
 //Debug Vars
 inline LPCSTR  CLASS_NAME  = "CustomClass";
@@ -36,4 +29,24 @@ inline WNDCLASSEXA windclass =
     .lpszClassName = CLASS_NAME,
     .hIconSm = LoadIconA(nullptr, IDI_APPLICATION),
     };
+inline Window & window = Window::InstanceWindow();
 inline LARGE_INTEGER start_count, end_count, counts, freq, fps;
+
+// OpenGl Vars
+
+inline GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    
+inline Shader Tring(SHADER(Traingl));
+
+inline GLint xL = glGetUniformLocation(Tring.GetProgram(), "u_offx");
+inline GLint yL = glGetUniformLocation(Tring.GetProgram(), "u_offy");
+inline GLint zL = glGetUniformLocation(Tring.GetProgram(), "u_offz");
+
+inline GLfloat xyz[3] = {0.0f, 0.0f, 0.0f};
+
+inline VAO vao;
+inline VBO vbo(vertices, sizeof(vertices));
