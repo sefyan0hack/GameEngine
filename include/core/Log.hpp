5291459d909a5b07c580f8f043203d3121f1af8d
sefyan0hack <sefyanhack@gmail.com>
2025-01-28 05:33:37 +0100

    improving Log

--- include/core/Log.hpp
+++ include/core/Log.hpp
@@ -3,6 +3,9 @@
 #include <source_location>
 #include <format>
 #include <iostream>
+#include <chrono>
+#include <string_view>
+#include <cstdlib>
 
 namespace {
 
@@ -10,37 +13,60 @@ enum class Log_LvL {
   ERR,
   WAR,
   INF,
-  PRT
 };
+
+static std::string formatTime(const std::chrono::system_clock::time_point& tp) {
+  return std::format("{:%Y-%m-%d %H:%M:%OS}", tp);
+}
+
+static std::string_view levelToString(Log_LvL level) {
+    switch (level) {
+        case Log_LvL::INF: return "INFO";
+        case Log_LvL::WAR: return "WARNING";
+        case Log_LvL::ERR: return "ERROR";
+        default:           return "UNKNOWN";
+    }
+}
 
 template <Log_LvL lvl, typename ...Ts>
 struct ERRF
 {
   ERRF(const std::format_string<Ts...> fmt, Ts&& ... ts, std::source_location loc = std::source_location::current())
   {
+    const auto now = std::chrono::system_clock::now();
     [[maybe_unused]] auto formatted_msg = std::format(fmt, std::forward<Ts>(ts)...);
+    auto lvl_str = levelToString(lvl);
 
+    std::cout << std::format("[{}] {} : {}\n\t --> {}:{}", lvl_str, formatTime(now), formatted_msg, loc.file_name(), loc.line()) << "\n";
     if constexpr (lvl == Log_LvL::ERR){
-      std::cout << std::format("<X> [ERROR] {}:{} -> {}", loc.file_name(), loc.line(), formatted_msg) << "\n";
-      std::abort();
-    }else if constexpr (lvl == Log_LvL::INF){
-      std::cout << std::format("<?> [INFO] {}:{} -> {}", loc.file_name(), loc.line(), formatted_msg) << "\n";
-
-    }else if constexpr (lvl == Log_LvL::WAR){
-      std::cout << std::format("<!> [WARNING] {}:{} -> {}", loc.file_name(), loc.line(), formatted_msg) << "\n";
-
-    }else if constexpr (lvl == Log_LvL::PRT){
-      std::cout << formatted_msg << "\n";
+      exit(EXIT_FAILURE);
     }
   }
+
 };
 
 template <Log_LvL lvl, typename... Ts>
 ERRF(const std::format_string<Ts...>, Ts&& ...) -> ERRF<lvl, Ts...>;
 
 }
+
 namespace Log
 {
+
+template <typename ...Ts>
+struct print{
+  print(const std::format_string<Ts...> fmt, Ts&& ... ts)
+  {
+    const auto now = std::chrono::system_clock::now();
+    [[maybe_unused]] auto formatted_msg = std::format(fmt, std::forward<Ts>(ts)...);
+
+    std::cout << std::format("{} : {}", formatTime(now), formatted_msg) << "\n";
+  }
+};
+
+template <typename... Ts>
+print(const std::format_string<Ts...>, Ts&& ...) -> print<Ts...>;
+
 
 template <typename ...Ts>
 using Error = ERRF<Log_LvL::ERR, Ts...>;
@@ -50,8 +76,5 @@ using Warning = ERRF<Log_LvL::WAR, Ts...>;
 
 template <typename ...Ts>
 using Info = ERRF<Log_LvL::INF, Ts...>;
-
-template <typename ...Ts>
-using print = ERRF<Log_LvL::PRT, Ts...>;
 
 } // namespace Log
