c22896b6bd5bd04302d6d6bc2180ea13aa2a9541
sefyan0hack <sefyanhack@gmail.com>
2025-01-28 11:48:51 +0100

    helper function

--- include/core/Utils.hpp
+++ include/core/Utils.hpp
@@ -12,3 +12,11 @@ auto setTimeOut( unsigned long delay, Function&& func, Args&&... args) -> void
         std::invoke(func, std::forward<Args>(args)...);
     }).detach();
 }
+
+// compile time
+constexpr std::string_view get_file_directory(const std::string_view file_path = __FILE__) {
+    const size_t last_slash = file_path.find_last_of("/\\");
+    return (last_slash == std::string_view::npos) ? "" : file_path.substr(0, last_slash);
+}
+
+constexpr std::string_view current_directory = get_file_directory();
