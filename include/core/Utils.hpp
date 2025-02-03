d99a854c0008e00fd6a10c2dd642f6550dcc8a4e
sefyan0hack <sefyanhack@gmail.com>
2025-02-03 17:00:55 +0100

    compile Time to Uper

--- include/core/Utils.hpp
+++ include/core/Utils.hpp
@@ -3,6 +3,10 @@
 #include <thread>
 #include <chrono>
 #include <functional>
+#include <array>
+#include <cstddef>
+#include <string_view>
+#include <algorithm>
 
 template<typename Function, typename... Args>
 auto setTimeOut( unsigned long delay, Function&& func, Args&&... args) -> void
@@ -24,3 +28,35 @@ constexpr auto is_odd(auto x) -> bool
 {
     return x % 2 != 0;
 }
+
+template <std::size_t N>
+struct FixedString {
+    constexpr FixedString(const char (&str)[N]) {
+        std::copy_n(str, N, value);
+    }
+    char value[N];
+};
+
+template <FixedString Str>
+struct ToUpper {
+    static constexpr auto compute() {
+        constexpr std::size_t N = sizeof(Str.value);
+        std::array<char, N> result{};
+        for (std::size_t i = 0; i < N - 1; ++i) {
+            char c = Str.value[i];
+            if (c >= 'a' && c <= 'z') {
+                result[i] = c - ('a' - 'A');
+            } else {
+                result[i] = c;
+            }
+        }
+        result[N - 1] = '\0';
+        return result;
+    }
+    static constexpr std::array<char, sizeof(Str.value)> value = compute();
+};
+
+template <FixedString Str>
+constexpr std::array<char, sizeof(Str.value)> ToUpper<Str>::value;
+
+#define TO_UPPER(str) std::string_view(ToUpper<FixedString{str}>::value.data())
