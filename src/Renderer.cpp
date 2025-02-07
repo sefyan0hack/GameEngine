c546a21df2896452dfeaf66739666322ed5979ba
sefyan0hack <sefyanhack@gmail.com>
2025-02-07 04:22:52 +0100

    render sky box first

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -4,6 +4,7 @@
 #include <core/Scene.hpp>
 #include <core/GameObject.hpp>
 #include <core/Camera.hpp>
+#include <ranges>
 
 Renderer::Renderer() {
 }
@@ -15,7 +16,23 @@ auto Renderer::render(Scene &scene, Camera &camera) -> void
 {
     //Drwaing    
     auto objs = scene.GetGameObjects();
-    for(auto &obj: objs){
+
+    // render sky box
+    auto skybox = objs.front();
+    auto skymaterial = skybox.GetMaterial();
+    auto& mesh = skybox.GetMesh();
+
+    glDepthFunc(GL_LEQUAL);
+    skybox.Bind();
+    skymaterial->Use();
+    skymaterial->SetUniform("Perspective", camera.GetPerspective());
+    skymaterial->SetUniform("View", glm::mat4(glm::mat3(camera.GetView())));
+    mesh.EnableAttribs();
+    draw(mesh);
+    mesh.DisableAttribs();
+    glDepthFunc(GL_LESS);
+
+    for(auto &obj: objs | std::views::drop(1)){
         obj.Bind();
         auto material = obj.GetMaterial();
         material->Use();
@@ -23,7 +40,9 @@ auto Renderer::render(Scene &scene, Camera &camera) -> void
         material->SetUniform("View", camera.GetView());
         auto sizeIns = static_cast<GLsizei>(obj.GetInstancePos().size());
         auto& mesh = obj.GetMesh();
+        mesh.EnableAttribs();
         draw(mesh, sizeIns);
+        mesh.DisableAttribs();
     }
 }
 
