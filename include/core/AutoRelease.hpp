9412fdccfcd1fe8876065b01f706b3362b89d749
sefyan0hack <sefyanhack@gmail.com>
2025-02-11 14:24:40 +0100

    add autoRelease Class

---
+++ include/core/AutoRelease.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <atomic>
+#include <cstddef>
+#include <utility>
+#include <functional>
+#include <memory>
+
+
+template <typename T>
+class AutoRelease {
+private:
+    T resource{};
+    std::function<void(T)> deleter;
+    std::shared_ptr<std::atomic<size_t>> ref_count;
+
+public:
+    AutoRelease(T res, std::function<void(T)> del) noexcept
+        : resource(res), deleter(std::move(del)), ref_count(std::make_shared<std::atomic<size_t>>(1)) {}
+
+    AutoRelease(AutoRelease&& other) noexcept
+        : resource(other.resource), deleter(std::move(other.deleter)), ref_count(std::move(other.ref_count)) {
+        other.resource = 0;
+    }
+
+    AutoRelease& operator=(AutoRelease&& other) noexcept {
+        if (this != &other) {
+            release();
+            resource = other.resource;
+            deleter = std::move(other.deleter);
+            ref_count = std::move(other.ref_count);
+            other.resource = 0;
+        }
+        return *this;
+    }
+
+    AutoRelease(const AutoRelease&) = default;
+    AutoRelease& operator=(const AutoRelease&) = default;
+
+    ~AutoRelease() {
+        release();
+    }
+
+    void release() noexcept {
+        if (ref_count && --(*ref_count) == 0) {
+            deleter(resource);
+            resource = 0;
+        }
+    }
+
+    T get() const noexcept {
+        return resource;
+    }
+
+    operator T() const noexcept {
+        return resource;
+    }
+
+    size_t use_count() const noexcept {
+        return ref_count ? ref_count->load(std::memory_order_relaxed) : 0;
+    }
+};
