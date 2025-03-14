d893ca061bf9a2f75061df4d68bcdc9a87360723
sefyan0hack <sefyanhack@gmail.com>
2025-03-14 00:10:06 +0000

    Log error message when file opening fails in load_file_async

--- src/Utils.cpp
+++ src/Utils.cpp
@@ -1,10 +1,11 @@
 #include <core/Utils.hpp>
+#include <core/Log.hpp>
 
 std::future<std::vector<char>> load_file_async(const std::string& filename) {
     return std::async(std::launch::async, [filename]() -> std::vector<char> {
         std::ifstream file(filename, std::ios::binary | std::ios::ate);
         if (!file) {
-            throw std::runtime_error("Failed to open file");
+            Log::Error("Failed to open file {}", filename);
         }
         std::streamsize size = file.tellg();
         file.seekg(0, std::ios::beg);
