d638039ae2a4a99ce83c77d0219ea7cfb51a8d97
sefyan0hack <sefyanhack@gmail.com>
2025-04-14 18:22:44 +0100

    remove `{}`

--- src/XWindow.cpp
+++ src/XWindow.cpp
@@ -20,7 +20,7 @@ CWindow::CWindow(int m_Width, int m_Height, const char* Title)
     #ifdef __WIN32
     m_OpenGl = std::make_shared<OpenGL>(m_WindowHandle);
     #elif defined(__linux__)
-    m_OpenGl = std::make_shared<OpenGL>({m_Display, m_WindowHandle});
+    m_OpenGl = std::make_shared<OpenGL>(m_Display, m_WindowHandle);
     #endif //__WIN32
 }
 
