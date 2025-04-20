705027d6e91e079b3b85e0aa09c3a475565b47fc
sefyan0hack <sefyanhack@gmail.com>
2025-04-20 21:53:50 +0100

    add some test casses

--- tests/Utils_test.cpp
+++ tests/Utils_test.cpp
@@ -6,7 +6,14 @@ TEST(load_file_async, exist) {
     auto r = load_file_async(__FILE__).get();
     EXPECT_EQ(r.has_value(), true);
 }
+
 TEST(load_file_async, notexist) {
     auto r = load_file_async("filenotexist.ext").get();
     EXPECT_EQ(r.has_value(), false);
+}
+
+TEST(split, string) {
+    auto r = split("hi my name sofyane", " ");
+    decltype(r) expect = {"hi", "my", "name", "sofyane"};
+    EXPECT_EQ(r, expect);
 }
