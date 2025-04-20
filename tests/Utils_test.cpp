0f4223d0e35798034e04377d424d7fd67dd58c4c
sefyan0hack <sefyanhack@gmail.com>
2025-04-20 23:49:07 +0100

    add test casess

--- tests/Utils_test.cpp
+++ tests/Utils_test.cpp
@@ -7,7 +7,7 @@ TEST(load_file_async, exist) {
     EXPECT_EQ(r.has_value(), true);
 }
 
-TEST(load_file_async, notexist) {
+TEST(load_file_async, not_exist) {
     auto r = load_file_async("filenotexist.ext").get();
     EXPECT_EQ(r.has_value(), false);
 }
@@ -16,4 +16,25 @@ TEST(split, string) {
     auto r = split("hi my name sofyane", " ");
     decltype(r) expect = {"hi", "my", "name", "sofyane"};
     EXPECT_EQ(r, expect);
+}
+
+TEST(split, empty_string) {
+    auto r = split("", " ");
+    decltype(r) expect;
+    EXPECT_EQ(r, expect);
+}
+
+TEST(replace, with_char) {
+    auto r = replace("fuzz", 'f', 'b');
+    EXPECT_EQ(r, "buzz");
+}
+
+TEST(replace, with_null_char) {
+    auto r = replace("fuzz", 'f', '\0');
+    EXPECT_EQ(r, "fuzz");
+}
+
+TEST(replace, with_not_exist_char) {
+    auto r = replace("fuzz", 'a', 'b');
+    EXPECT_EQ(r, "fuzz");
 }
