cd2edadb2a11e29cff00e4d519d4f18ddd7a9c49
sefyan0hack <sefyanhack@gmail.com>
2025-04-23 20:54:17 +0100

    remove format test from transforme

--- tests/Transform_test.cpp
+++ tests/Transform_test.cpp
@@ -3,18 +3,10 @@
 
 using namespace std;
 using namespace glm;
+
 TEST(Transform, ctor) {
     auto r = Transform(vec3());
     EXPECT_EQ(r.position, vec3());
     EXPECT_EQ(r.rotation, vec3());
     EXPECT_EQ(r.scale, vec3(1.0f, 1.0f, 1.0f));
-}
-
-TEST(Transform, formatter) {
-    auto r = format("{}", Transform(vec3()));
-    auto e = format(
-        R"({{ "position": {}, "rotation": {}, "scale": {} }})", 
-        vec3(), vec3(), vec3(1.0f, 1.0f, 1.0f)
-    );
-    EXPECT_EQ(r, e);
 }
