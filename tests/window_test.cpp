382bf23f0e58aaa5a04bbc10136b06c1094bfdaf
sefyan0hack <sefyanhack@gmail.com>
2025-04-30 20:56:22 +0100

    simpl CWindow test

---
+++ tests/window_test.cpp
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include <core/Window.hpp>
+#include <iostream>
+
+using namespace std;
+class CWindow_Test : public ::testing::Test {
+    public:
+    static int GetWidth(const CWindow& window) { return window.m_Width; }
+    static int GetHeight(const CWindow& window) { return window.m_Height; }
+};
+
+TEST_F(CWindow_Test, ctor){
+    auto r = CWindow(100, 100, "Window");
+
+    EXPECT_GE(GetWidth(r), 100);
+    EXPECT_LE(GetHeight(r), 100);
+}
