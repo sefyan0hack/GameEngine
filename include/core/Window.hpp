6023f4892af4aabeafa48df458f8d78e391c9ea7
sefyan0hack <sefyanhack@gmail.com>
2025-05-01 17:03:51 +0100

    compine window  impls in one header

--- include/core/Window.hpp
+++ include/core/Window.hpp
@@ -5,7 +5,7 @@
 #include <core/Keyboard.hpp>
 #include <core/Mouse.hpp>
 
-#ifdef _WIN32
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #include <windows.h>
 #include <tchar.h>
 
@@ -74,4 +74,49 @@ class CWindow
         inline static unsigned short S_WindowsCount = 0;
 };
 
+#elif defined(__linux__)
+#include <X11/Xlib.h>
+
+class CWindow
+{
+  FOR_TEST(CWindow)
+    public:
+        CWindow(int Width, int Height, const char* Title);
+        CWindow(const CWindow& other);
+        CWindow(CWindow&&) = delete;
+    
+        auto operator = (const CWindow &) -> CWindow = delete ;
+        auto operator = (CWindow &&)      -> CWindow = delete ;
+    
+        ~CWindow();
+    
+    public:
+        // auto Hinstance() const   -> HINSTANCE ;
+        auto WindowHandle() const        -> Window ;
+        // auto DrawContext() const         -> HDC ;
+        auto Width() const       -> int ;
+        auto Height() const      -> int ;
+        auto opengl() const          -> std::shared_ptr<OpenGL> ;
+        auto Visible() const        -> bool ;
+        static auto WindowsCount()  -> unsigned short ;
+        auto ProcessMessages() -> void ;
+
+    private:
+        auto _init_helper(int Width, int Height, const char* Title)                         -> void ;
+    private:
+        Display* m_Display;
+        Window m_WindowHandle;
+        XEvent  m_Event;
+        int m_Width;
+        int m_Height;
+        bool m_Visible;
+        std::vector<std::byte> m_RawBuffer;
+        std::shared_ptr<OpenGL> m_OpenGl;
+    public:
+        std::shared_ptr<Keyboard> m_Keyboard;
+        std::shared_ptr<Mouse> m_Mouse;
+        
+    public:
+        inline static unsigned short S_WindowsCount = 0;
+};
 #endif //_WIN32
