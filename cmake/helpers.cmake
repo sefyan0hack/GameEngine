945bf4578487228e872146b42f278208b262144f
sefyan0hack <sefyanhack@gmail.com>
2025-05-03 02:03:46 +0100

    remove `Winline` from gcc/clang compile flags

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -101,7 +101,7 @@ function(apply_compile_options)
                 -Wall -Wextra -Wpedantic -Wconversion -Wfloat-equal -Wwrite-strings -Wno-sign-conversion
                 -fstack-protector-strong
                 -Wnull-dereference -Wswitch-enum
-                -Wuninitialized -Wpointer-arith -Wreturn-type -Winline -Wredundant-decls
+                -Wuninitialized -Wpointer-arith -Wreturn-type -Wredundant-decls
                 -fno-operator-names
                 -D_GLIBCXX_HAVE_STACKTRACE
                 # Debug flags
