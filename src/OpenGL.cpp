66f8fc149b46dbc11f298266fdbc7f4388b9fa50
sefyan0hack <sefyanhack@gmail.com>
2025-05-11 16:43:04 +0100

    update opengl.cpp

--- src/OpenGL.cpp
+++ src/OpenGL.cpp
@@ -223,7 +223,7 @@ auto OpenGL::init_opengl_linux(Window window) -> void
     #define GLX_CONTEXT_CORE_PROFILE_BIT_ARB  0x00000001
     #define GLX_CONTEXT_COMPATIBILITY_PROFILE_BIT_ARB 0x00000002
     #define GLX_CONTEXT_PROFILE_MASK_ARB      0x9126
-    
+
     int contextAttribs[] = {
         GLX_CONTEXT_MAJOR_VERSION_ARB, 4,
         GLX_CONTEXT_MINOR_VERSION_ARB, 4,
@@ -231,8 +231,7 @@ auto OpenGL::init_opengl_linux(Window window) -> void
         0
     };
 
-    PFNGLXCREATECONTEXTATTRIBSARBPROC glXCreateContextAttribsARB = 
-        (PFNGLXCREATECONTEXTATTRIBSARBPROC)glXGetProcAddressARB((const GLubyte*)"glXCreateContextAttribsARB");
+    glXCreateContextAttribsARB = (decltype(glXCreateContextAttribsARB))glXGetProcAddressARB((const GLubyte*)"glXCreateContextAttribsARB");
 
     m_Context = glXCreateContextAttribsARB(m_MainHDC, fbc[0], nullptr, True, contextAttribs);
     XFree(fbc);
