8cb18825cbe658fd4d6a033ade246d55a35c61fe
sefyan0hack <sefyanhack@gmail.com>
2025-05-31 13:35:28 +0100

    add CWindow class tests

--- tests/unit/window_test.cpp
+++ tests/unit/window_test.cpp
@@ -1,28 +1,46 @@
 #include <gtest/gtest.h>
 #include <core/Window.hpp>
-#include <iostream>
-#ifdef WINDOWS_PLT
-#include <windows.h>
+
 using namespace std;
-// class CWindow_Test : public ::testing::Test {
-//     public:
-//     static auto WindowHandle(const CWindow& w)  -> HWND      { return w.m_WindowHandle; }
-//     static auto DrawContext(const CWindow& w)   -> HDC       { return w.m_HDC; }
-//     static auto Width(const CWindow& w)         -> int       { return w.m_Width; }
-//     static auto Height(const CWindow& w)        -> int       { return w.m_Height; }
-//     static auto opengl(const CWindow& w)        -> std::shared_ptr<OpenGL> { return w.m_OpenGl; }
-//     static auto Visible(const CWindow& w)       -> bool { return w.m_Visible; }
-//     static auto WindowsCount()                  -> unsigned short { return CWindow::S_WindowsCount; }
-// };
 
-// TEST_F(CWindow_Test, ctor){
-//     auto r = CWindow(100, 100, "Window");
+using CWindow_Test_Ctor1 = CWindow::Test<1>;
 
-//     EXPECT_NE(Hinstance(r), nullptr);
-//     EXPECT_NE(WindowHandle(r), nullptr);
-//     EXPECT_NE(DrawContext(r), nullptr);
-//     EXPECT_EQ(opengl(r)->isValid(), true);
-//     EXPECT_EQ(Visible(r), false);
-//     EXPECT_EQ(WindowsCount(), 1);
-// }
-#endif
+#define TEST_INTERFACE \
+    MEMBER_VAR(m_WindowHandle);\
+    MEMBER_VAR(m_DrawContext);\
+    MEMBER_VAR(m_Width);\
+    MEMBER_VAR(m_Height);\
+    MEMBER_VAR(m_Visible);\
+    MEMBER_VAR(m_RawBuffer);\
+    MEMBER_VAR(m_OpenGl);\
+    MEMBER_VAR(m_refCount);\
+    MEMBER_VAR(m_Keyboard);\
+    MEMBER_VAR(m_Mouse);\
+    MEMBER_VAR(S_WindowsCount);\
+    \
+    MEMBER_FUN(WindowHandle);\
+    MEMBER_FUN(DrawContext);\
+    MEMBER_FUN(Width);\
+    MEMBER_FUN(Height);\
+    MEMBER_FUN(opengl);\
+    MEMBER_FUN(Visible);\
+    MEMBER_FUN(Show);\
+    MEMBER_FUN(Hide);\
+    MEMBER_FUN(WindowsCount);
+
+
+template<>
+struct CWindow::Test<1> : public ::testing::Test {
+    CWindow member;
+    Test() : member(CWindow(100, 100, "Window")){}
+    TEST_INTERFACE
+};
+
+TEST_F(CWindow_Test_Ctor1, ctor){
+
+    EXPECT_NE(m_WindowHandle, nullptr);
+    EXPECT_NE(m_DrawContext, nullptr);
+    EXPECT_EQ(m_OpenGl->isValid(), true);
+    EXPECT_EQ(m_Visible, false);
+    EXPECT_EQ(S_WindowsCount, 1);
+}
