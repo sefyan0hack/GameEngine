77d9d48c7a5b5402b35bc0db80a76386f672a1e0
sefyan0hack <sefyanhack@gmail.com>
2025-06-07 03:11:54 +0100

    add some coverage flags

--- cmake/helpers.cmake
+++ cmake/helpers.cmake
@@ -127,15 +127,24 @@ function(apply_coverage_options)
                 "$<$<BOOL:${ENABLE_COVERAGE}>:-fcoverage-mapping>"
                 "$<$<BOOL:${ENABLE_COVERAGE}>:-fcoverage-mcdc>"
             )
+            target_link_options(${target} PRIVATE
+                "$<$<BOOL:${ENABLE_COVERAGE}>:-Wl,--bigobj>"  # Critical for large sections
+            )
         elseif(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
             target_compile_options(${target} PRIVATE
                 "$<$<BOOL:${ENABLE_COVERAGE}>:--coverage>"
-                "$<$<BOOL:${ENABLE_COVERAGE}>:-fprofile-arcs>"
-                "$<$<BOOL:${ENABLE_COVERAGE}>:-ftest-coverage>"
                 "$<$<BOOL:${ENABLE_COVERAGE}>:-fno-omit-frame-pointer>"
+                "$<$<BOOL:${ENABLE_COVERAGE}>:-fno-elide-constructors>" # Track copy/move constructors
+                "$<$<BOOL:${ENABLE_COVERAGE}>:-fkeep-inline-functions>"  # Track inline/template functions
+                "$<$<BOOL:${ENABLE_COVERAGE}>:-fprofile-abs-path>" # Absolute paths for gcovr
                 "$<$<BOOL:${ENABLE_COVERAGE}>:-fno-inline>"
+                "$<$<BOOL:${ENABLE_COVERAGE}>:-Wa,-mbig-obj>"  # Direct assembler support
+            )
+            target_link_options(${target} PRIVATE
+                "$<$<BOOL:${ENABLE_COVERAGE}>:-Wl,--bigobj>"  # Critical for large sections
             )
         endif()
+
     endforeach()
 endfunction()
 
