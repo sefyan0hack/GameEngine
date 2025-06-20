2a0b20ca06f60076663af62db87b3ff783fe3945
sefyan0hack <sefyanhack@gmail.com>
2025-06-20 05:25:05 +0100

    add new member file to Shader class

--- include/core/Shader.hpp
+++ include/core/Shader.hpp
@@ -19,12 +19,15 @@ class Shader
       auto id() const                -> GLuint ;
       auto Type() const              -> GLenum ;
       auto TypeName() const          -> const char* ;
+      auto File() const              -> std::string;
       auto Content() const           -> std::vector<GLchar>;
-
+      
+      
       static auto LoadFile(const char* filename) -> std::vector<GLchar>;
       static auto Compile(GLuint shader)             -> void;
       static auto LoadSource(const std::vector<GLchar>& src, GLuint shader) -> void;
-      static auto checkShaderCompileStatus(const GLuint &shader) -> void;
+      static auto checkShaderCompileStatus(const Shader &shader) -> void;
+      static auto GetShaderInfo(GLuint id, GLenum what) -> std::optional<GLint>; //what : GL_SHADER_TYPE, GL_DELETE_STATUS, GL_COMPILE_STATUS, GL_INFO_LOG_LENGTH, GL_SHADER_SOURCE_LENGTH.
 
   private:
       auto LoadSource()                   -> void;
@@ -32,6 +35,7 @@ class Shader
   private:
       GLuint m_Id;
       GLenum m_Type;
+      std::string m_File;
       std::vector<GLchar> m_Content;
       FOR_TEST
 };
--- src/Shader.cpp
+++ src/Shader.cpp
@@ -11,10 +11,7 @@ constexpr auto to_string(GLenum type) -> const char*
   switch(type){
     case GL_VERTEX_SHADER: return "GL_VERTEX_SHADER";
     case GL_FRAGMENT_SHADER: return "GL_FRAGMENT_SHADER";
-    case GL_COMPUTE_SHADER: return "GL_COMPUTE_SHADER";
     case GL_GEOMETRY_SHADER: return "GL_GEOMETRY_SHADER";
-    case GL_TESS_CONTROL_SHADER: return "GL_TESS_CONTROL_SHADER";
-    case GL_TESS_EVALUATION_SHADER: return "GL_TESS_EVALUATION_SHADER";
     default: return "UNKNOWN";
   }
 }
@@ -22,27 +19,30 @@ constexpr auto to_string(GLenum type) -> const char*
 }
 
 Shader::Shader()
-: m_Id(0), m_Type(0), m_Content({})
+: m_Id(0), m_Type(0), m_File(""), m_Content({})
 {
 }
 
 Shader::Shader(const char* name, GLenum type)
-: m_Id(gl::CreateShader(type)), m_Type(type), m_Content(LoadFile(name))
+    : m_Id(gl::CreateShader(type))
+    , m_Type(type)
+    , m_File(name)
+    , m_Content(LoadFile(name))
 {
     LoadSource();
     Compile(m_Id);
-    checkShaderCompileStatus(m_Id);
-    
+    checkShaderCompileStatus(*this);
 }
 
 Shader::Shader(const Shader& other)
     : m_Id(gl::CreateShader(other.m_Type))
     , m_Type(other.m_Type)
+    , m_File(other.m_File)
     , m_Content(other.m_Content)
 {
     LoadSource();
     Compile(m_Id);
-    checkShaderCompileStatus(m_Id);
+    checkShaderCompileStatus(*this);
     
 
 }
@@ -50,6 +50,7 @@ Shader::Shader(const Shader& other)
 Shader::Shader(Shader&& other) noexcept
     : m_Id(std::exchange(other.m_Id, 0))
     , m_Type(std::exchange(other.m_Type, 0))
+    , m_File(std::exchange(other.m_File, ""))
     , m_Content(std::exchange(other.m_Content, {}))
 {
 }
@@ -59,19 +60,22 @@ Shader &Shader::operator=(const Shader& other)
     if(*this != other){
         this->m_Id = gl::CreateShader(other.m_Type);
         this->m_Type = other.m_Type;
+        this->m_File = other.m_File;
         this->m_Content = other.m_Content;
         
         LoadSource();
         Compile(m_Id);
-        checkShaderCompileStatus(m_Id);
+        checkShaderCompileStatus(*this);
     }
     
     return *this;
 }
+
 bool Shader::operator==(const Shader &other)
 {
     return this->m_Id == other.m_Id;
 }
+
 Shader::~Shader()
 {
     gl::DeleteShader(m_Id);
@@ -105,7 +109,7 @@ auto Shader::LoadFile(const char* filename) -> std::vector<GLchar>
     buffer << std::format("#version {} {}\n", glsl_verion, sys::Target == sys::Target::Web ? "es" : "core");
     buffer << std::format("precision {} float;\n", sys::Target == sys::Target::Web ? "mediump" : "highp");
 
-    //todo : size increment insted of  buff.str()
+    //todo : size increment insted of buff.str()
     auto op = file_to_str(filename);
     if(op)
     {
@@ -128,19 +132,23 @@ auto Shader::Compile(GLuint shader) -> void
     gl::CompileShader(shader);
 }
 
-auto Shader::checkShaderCompileStatus(const GLuint &shader) -> void
+auto Shader::checkShaderCompileStatus(const Shader &shader) -> void
 {
-    GLint success = 0;
-    gl::GetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    auto id = shader.id();
+    GLint success = GetShaderInfo(id, GL_COMPILE_STATUS).value_or(0);
+
     if (!success) {
-        GLchar infoLog[512];
-        std::memset(infoLog, '\0', 512);
-        gl::GetShaderInfoLog(shader, 512, NULL, infoLog);
-        gl::DeleteShader(shader);
-        Error("SHADER::COMPILATION_FAILED {}", infoLog);
+        GLint infologlength = GetShaderInfo(id, GL_INFO_LOG_LENGTH).value_or(0);
+
+        if(infologlength > 0){
+            std::string infoLog(infologlength, '\0');
+
+            gl::GetShaderInfoLog(id, infologlength, NULL, infoLog.data());
+            gl::DeleteShader(id);
+            Error("[{}] {} \n", shader.File(), infoLog);
+        }
     }
 }
-
 
 auto Shader::id() const -> GLuint
 {
@@ -157,7 +165,27 @@ auto Shader::TypeName() const -> const char*
     return to_string(m_Type);
 }
 
+auto Shader::File() const -> std::string
+{
+    return m_File;
+}
+
 auto Shader::Content() const -> std::vector<GLchar>
 {
     return m_Content;
+}
+
+auto Shader::GetShaderInfo(GLuint id, GLenum what) -> std::optional<GLint>
+{
+    auto maxGLint = std::numeric_limits<GLint>::max();
+    GLint result = maxGLint;
+
+    gl::GetShaderiv(id, what, &result);
+
+    if(result != maxGLint){
+        return result;
+    }
+    else{
+        return std::nullopt;
+    }
 }
