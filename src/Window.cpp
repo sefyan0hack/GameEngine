427c33c8ecc6c72a702a61f7cdb9d206bda28149
sefyan0hack <sefyanhack@gmail.com>
2025-07-15 09:27:36 +0100

    [skip CI]

--- src/Window.cpp
+++ src/Window.cpp
@@ -368,6 +368,7 @@ auto CWindow::new_window(int32_t Width, int32_t Height, const char* Title) -> st
 
 	emscripten_set_window_title(Title);
 	emscripten_set_canvas_element_size(window_handle, Width, Height);
+	emscripten_run_script(std::format("document.querySelector('{}').focus();", window_handle).c_str());
 
 	return {window_handle, DrawContext};
 }
