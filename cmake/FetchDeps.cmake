efd69ad2e81e1467a6680417d051e715c284f141
sefyan0hack <sefyanhack@gmail.com>
2025-07-15 08:42:29 +0100

    typo [skip CI]

--- cmake/FetchDeps.cmake
+++ cmake/FetchDeps.cmake
@@ -19,20 +19,15 @@ set(STB_IMG_HEADER ${stb_image_header_SOURCE_DIR})
 add_library(stb_image INTERFACE)
 target_include_directories(stb_image SYSTEM INTERFACE ${STB_IMG_HEADER})
 
-CPMAddPackage(
-    NAME khr
-    URL https://raw.githubusercontent.com/KhronosGroup/EGL-Registry/main/api/KHR/khrplatform.h
-    DOWNLOAD_ONLY YES
-    DOWNLOAD_NO_EXTRACT TRUE
-    DOWNLOAD_NAME khrplatform.h
-)
+
+add_single_file(khrplatform https://raw.githubusercontent.com/KhronosGroup/EGL-Registry/main/api/KHR/khrplatform.h)
 
 if(NOT EXISTS ${CMAKE_CURRENT_BINARY_DIR}/KHR/khrplatform.h)
     file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/KHR)
-    include_directories(${CMAKE_CURRENT_BINARY_DIR}/KHR)
+    include_directories(${CMAKE_CURRENT_BINARY_DIR})
 
     file(RENAME
-        ${CMAKE_CURRENT_BINARY_DIR}/_deps/khr-src/khrplatform.h 
+        ${CMAKE_CURRENT_BINARY_DIR}/_deps/khr-src/khrplatform.h
         ${CMAKE_CURRENT_BINARY_DIR}/KHR/khrplatform.h
     )
 endif()
