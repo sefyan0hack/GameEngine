c229b0f30016660b3767153cdad4460a8ad63ba8
sefyan0hack <sefyanhack@gmail.com>
2025-07-19 15:12:24 +0100

    add Use full conceps for Event

--- include/core/Event.hpp
+++ include/core/Event.hpp
@@ -20,7 +20,15 @@ using Event = std::variant<
     CWindow::QuitEvent
 >;
 
-constexpr auto Events()
-{
-    return utils::Variant_to_Array<Event>();
-}
+constexpr auto Events = utils::Variant_to_Array<Event>();
+
+template <typename T>
+concept EventType = std::is_same_v<T, std::monostate> || ::type_name<T>().ends_with("Event");
+
+template <typename T>
+concept ISEvent = Variant<T> && [](){
+    using V = std::remove_cvref_t<T>;
+    return []<std::size_t... I>(std::index_sequence<I...>) {
+        return (EventType<std::variant_alternative_t<I, V>> && ...);
+    }(std::make_index_sequence<std::variant_size_v<V>>{});
+}();
