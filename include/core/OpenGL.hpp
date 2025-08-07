18ffaa16ae74037ca520013070918065692f92c6
sefyan0hack <sefyanhack@gmail.com>
2025-08-07 02:33:40 +0100

    yned

--- include/core/OpenGL.hpp
+++ include/core/OpenGL.hpp
@@ -26,6 +26,8 @@ namespace gl {
     constexpr int32_t  GLMajorVersion   = 3;
     constexpr int32_t  GLMinorVersion   = 3;
 
+    auto GetInteger(GLenum name) -> GLuint;
+
 class OpenGL
 {
     public:
@@ -48,7 +50,6 @@ class OpenGL
         auto CreationTime() const -> std::time_t;
         auto HasExtension(const std::string& ext) const -> bool;
 
-        auto GetInteger(GLenum name) -> GLint;
 
         static auto Vendor() -> std::string;
         static auto Renderer() -> std::string;
