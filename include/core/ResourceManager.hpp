02fe2809d474fc0ab946749dc43952f0f73bb811
sefyan0hack <sefyanhack@gmail.com>
2025-08-08 04:57:08 +0100

    improve ResourceMnagaer

--- include/core/ResourceManager.hpp
+++ include/core/ResourceManager.hpp
@@ -4,24 +4,46 @@
 #include <core/Shader.hpp>
 #include <core/Log.hpp>
 
-
 class ResourceManager
 {
+private:
+    ResourceManager() = default;
+    ~ResourceManager() = default;
+    class Proxy {
+    public:
+        Proxy(const std::string_view& key) : m_key(key) {}
+
+        template <class T>
+        void operator=(T&& obj) {
+            ResourceManager::getInstance().m_Resources[m_key] = std::make_shared<std::remove_reference_t<T>>(std::forward<T>(obj));
+        }
+
+        template <class T>
+        operator std::shared_ptr<T>() const {
+            return ResourceManager::getInstance().get<T>(m_key);
+        }
+
+        template <class T>
+        std::shared_ptr<T> get() const {
+            return ResourceManager::getInstance().get<T>(m_key);
+        }
+
+    private:
+        std::string_view m_key;
+    };
 public:
     ResourceManager(const ResourceManager&) = delete;
     ResourceManager& operator=(const ResourceManager&) = delete;
 
-
-    template <class T>
-    inline auto load(const std::string &path) -> void
+    auto operator[](const std::string_view& key) -> Proxy
     {
-        m_Resources[path] = std::make_shared<T>(path);
+        return Proxy(key);
     }
 
     template <class T>
-    auto get(const std::string &path) -> std::shared_ptr<T>
+    auto get(const std::string_view &path) -> std::shared_ptr<T>
     {
-        return std::any_cast<std::shared_ptr<T>>(m_Resources.at(path));
+        return std::static_pointer_cast<T>(m_Resources.at(path));
     }
 
     static auto getInstance() -> ResourceManager&
@@ -31,11 +53,8 @@ public:
     }
 
 private:
-    // Make the constructor and destructor private to restrict instantiation
-    ResourceManager() = default;
-    ~ResourceManager() = default;
 
-    std::unordered_map<std::string, std::any> m_Resources;
+    std::unordered_map<std::string_view, std::shared_ptr<void>> m_Resources;
 
     FOR_TEST
 };
