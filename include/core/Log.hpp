b96bbb49852e73eb8ea0cd82beb51be9a1acf44f
sefyan0hack <sefyanhack@gmail.com>
2025-08-12 08:43:52 +0100

    empty `stacktrace` is not impimened by compiler

--- include/core/Log.hpp
+++ include/core/Log.hpp
@@ -1,12 +1,38 @@
 #pragma once
 
 #include <source_location>
-#include <stacktrace>
 #include <iostream>
 #include <cstdlib>
 #include <format>
 #include <chrono>
 #include <exception>
+
+#if defined(WEB_PLT) || !__has_include(<stacktrace>)
+namespace std {
+  struct stacktrace {
+    static stacktrace current() noexcept { return {}; }
+    static stacktrace current([[maybe_unused]] const size_t Skip) noexcept { return {}; }
+    size_t size() { return 0; }
+  };
+
+  inline auto to_string([[maybe_unused]] const stacktrace& st ) -> string
+  {
+    return "no_stack_trace";
+  }
+
+  template<>
+  struct formatter<stacktrace> {
+    constexpr auto parse(std::format_parse_context& context) {
+      return context.begin();
+    }
+    auto format(const stacktrace& obj, format_context& context) const {
+      return format_to(context.out(),"{}", to_string(obj));
+    }
+  };
+}
+#else
+  #include <stacktrace>
+#endif
 
 namespace Debug {
 
@@ -52,19 +78,11 @@ public:
     : std::runtime_error(std::format(fmt, std::forward<Ts>(args)...))
     , m_Location(loc)
     , m_Timestamp(clock::now())
-    #ifdef __cpp_lib_stacktrace
-    , m_Trace(std::stacktrace::current(1))
-    #endif
   {}
   auto throwing_routine() const noexcept -> const char* { return m_Location.function_name(); }
 
   auto trace() const noexcept -> const char* { 
-    #ifdef __cpp_lib_stacktrace
-      return to_string(m_Trace).c_str();
-    #else
-      return "no_stack_trace";
-    #endif
-
+    return std::to_string(std::stacktrace::current(1)).c_str();
   }
   auto when()  const noexcept -> const char* { return std::format("{:%Y-%m-%d %H:%M:%OS}", m_Timestamp).c_str(); }
   auto where() const noexcept -> const char* { return std::format("{}:{}", m_Location.file_name(), m_Location.line()).c_str(); }
@@ -72,13 +90,14 @@ public:
 
   auto all() const noexcept -> const char*
   {
+    auto trc = std::stacktrace::current(1);
     return std::format(
       "{} :\n"
       "\t-> what : `{}`\n"
       "Stack Trace ({} Frame): {{\n{}}}\n",
       when(), what(),
       #ifdef __cpp_lib_stacktrace
-      m_Trace.size(), trace()
+      trc.size(), trace()
       #else
       "0", "no_stack_trace"
       #endif
@@ -88,9 +107,6 @@ public:
 private:
   std::source_location m_Location;
   clock::time_point m_Timestamp;
-  #ifdef __cpp_lib_stacktrace
-  std::stacktrace m_Trace;
-  #endif
 };
 
 #ifndef CException
