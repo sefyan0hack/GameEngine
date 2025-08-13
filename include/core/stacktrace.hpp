d9af78debe6df4068db6ebb1813383b1a1944b81
sefyan0hack <sefyanhack@gmail.com>
2025-08-13 13:26:36 +0100

    test STACKTRACE_SUPPORTED

--- include/core/stacktrace.hpp
+++ include/core/stacktrace.hpp
@@ -2,7 +2,7 @@
 #include <string>
 #include <array>
 
-#if __cpp_lib_stacktrace
+#if defined(STACKTRACE_SUPPORTED)
 
 #include <stacktrace>
 
@@ -20,43 +20,43 @@ inline auto to_string([[maybe_unused]] const stacktrace& st ) -> std::string
 }
 #else
 
-struct stacktrace_entry {
-  std::string description() const { return "??"; }
-  std::string source_file() const { return "??"; }
-  size_t source_line() const { return 0; }
-};
+// struct stacktrace_entry {
+//   std::string description() const { return "??"; }
+//   std::string source_file() const { return "??"; }
+//   size_t source_line() const { return 0; }
+// };
 
-struct stacktrace {
-  using const_iterator = std::array<stacktrace_entry, 0>::const_iterator;
+// struct stacktrace {
+//   using const_iterator = std::array<stacktrace_entry, 0>::const_iterator;
   
-  static stacktrace current() noexcept { return {}; }
-  static stacktrace current([[maybe_unused]] const size_t skip) noexcept { return {}; }
+//   static stacktrace current() noexcept { return {}; }
+//   static stacktrace current([[maybe_unused]] const size_t skip) noexcept { return {}; }
   
-  // Required members
-  bool empty() const noexcept { return m_entries.empty(); }
-  const_iterator begin() const noexcept { return m_entries.begin(); }
-  const_iterator end() const noexcept { return m_entries.end(); }
-  size_t size() const noexcept { return m_entries.size(); }
+//   // Required members
+//   bool empty() const noexcept { return m_entries.empty(); }
+//   const_iterator begin() const noexcept { return m_entries.begin(); }
+//   const_iterator end() const noexcept { return m_entries.end(); }
+//   size_t size() const noexcept { return m_entries.size(); }
   
-  const stacktrace_entry& operator[](size_t index) const { return m_entries[index]; }
+//   const stacktrace_entry& operator[](size_t index) const { return m_entries[index]; }
 
-private:
-  std::array<stacktrace_entry, 0> m_entries;
-};
+// private:
+//   std::array<stacktrace_entry, 0> m_entries;
+// };
 
-inline auto to_string([[maybe_unused]] const stacktrace& st ) -> std::string
-{
-  return std::format("Stack Trace ({} Frame): {{ }}", st.size());
-}
+// inline auto to_string([[maybe_unused]] const stacktrace& st ) -> std::string
+// {
+//   return std::format("Stack Trace ({} Frame): {{ }}", st.size());
+// }
 
-template<>
-struct std::formatter<stacktrace> {
-  constexpr auto parse(std::format_parse_context& context) {
-    return context.begin();
-  }
-  auto format(const stacktrace& obj, std::format_context& context) const {
-    return std::format_to(context.out(),"{}", to_string(obj));
-  }
-};
+// template<>
+// struct std::formatter<stacktrace> {
+//   constexpr auto parse(std::format_parse_context& context) {
+//     return context.begin();
+//   }
+//   auto format(const stacktrace& obj, std::format_context& context) const {
+//     return std::format_to(context.out(),"{}", to_string(obj));
+//   }
+// };
 
 #endif
