563a4cad9ee1df617b19f12f1367f55cbad074db
sefyan0hack <sefyanhack@gmail.com>
2025-11-06 11:10:05 +0100

    fix err

--- SandBox/main.cpp
+++ SandBox/main.cpp
@@ -12,7 +12,7 @@ int main() {
     game.load();
 
     auto create_and_run_game = game.get_function<void(*)()>("create_and_run_game");
-    if (!create_and_run_game) Exception("Can't load symbol `{}`: {}", "create_and_run_game", error());
+    if (!create_and_run_game) Exception("Can't load symbol `{}`: {}", "create_and_run_game", game.error());
 
     create_and_run_game();
 
