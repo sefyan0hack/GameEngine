2655b7cea2399d9661cb584e0b21d9021a1c25f6
sefyan0hack <sefyanhack@gmail.com>
2025-11-06 10:40:28 +0100

    typo

--- Engine/core/DynLib.hpp
+++ Engine/core/DynLib.hpp
@@ -15,9 +15,9 @@ class DynLib {
 public:
     DynLib(const char* lib) : m_handle(nullptr), m_file_path(lib), m_last_mod(mod_time()) {
         #if defined(WINDOWS_PLT)
-            m_file_path += ".dll"
+            m_file_path += ".dll";
         #elif defined(LINUX_PLT) || defined(WEB_PLT)
-            m_file_path += ".so"
+            m_file_path += ".so";
         #endif
     }
 
