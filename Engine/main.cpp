dfe76b5afa1f735916005132bc7324fb3fe8bbed
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2025-12-23 18:51:27 +0100

    Wait for the window handle to be valid

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -12,6 +12,17 @@ constexpr auto WINDOW_HIEGHT = 640;
 MAIN_FUNC {
     try {
         #if defined(ANDROID_PLT)
+        // Wait for the window handle to be valid
+        while (arg1->window == nullptr) {
+            int ident;
+            int events;
+            struct android_poll_source* source;
+            if ((ident = ALooper_pollAll(-1, nullptr, &events, (void**)&source)) >= 0) {
+                if (source != nullptr) source->process(arg1, source);
+            }
+            if (arg1->destroyRequested != 0) return; 
+        }
+        
         CWindow window(arg1->window);
         #else
         CWindow window(WINDOW_WIDTH, WINDOW_HIEGHT, "");
