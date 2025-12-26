ca9486236abb300bd902b0d8bd8c9e852483d2e5
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2025-12-26 23:16:39 +0100

    wait for window refactor to function

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -5,35 +5,29 @@
 #include <Engine.hpp>
 
 
+#if defined(ANDROID_PLT)
+bool wait_android_native_window(android_app* state) { // wait for native window and if destroyRequested return false
+    if (!state) return false;
+
+    while (state->window == nullptr && !state->destroyRequested) {
+        int events;
+        struct android_poll_source* source;
+        if (ALooper_pollOnce(-1, nullptr, &events, (void**)&source) >= 0) {
+            if (source) source->process(state, source);
+        }
+    }
+
+    return (state->window != nullptr && !state->destroyRequested);
+}
+#endif
+
 MAIN_FUNC {
     try {
         void* platform_state = nullptr;
 
         #if defined(ANDROID_PLT)
-        if(arg1 == nullptr) throw Exception("android_app is null");
-
-        auto state = arg1;
-
-        state->onAppCmd = [](struct android_app* app, int32_t cmd) {
-                if (cmd == APP_CMD_INIT_WINDOW) {
-                    // Signal that we are ready. 
-                    // We don't create APP here yet to keep logic out of the lambda.
-                }
-            };
-
-        while (state->window == nullptr) {
-            int events;
-            struct android_poll_source* source;
-
-            // -1 means block indefinitely until an event arrives
-            if (ALooper_pollOnce(-1, nullptr, &events, (void**)&source) >= 0) {
-                if (source) source->process(state, source);
-            }
-
-            if (state->destroyRequested) return;
-        }
-
-        platform_state = state;
+        platform_state = arg1;
+        if (!wait_android_native_window(arg1)) return;
         #endif
 
         APP app(platform_state);
