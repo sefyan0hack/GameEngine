d388f9c473b467a28ae8b01ceb154cb1d4721f4e
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2025-12-26 23:25:12 +0100

    improve APP::run() for android

--- Engine/core/platform/android/APP.cpp
+++ Engine/core/platform/android/APP.cpp
@@ -2,31 +2,17 @@
 
 auto APP::run() -> void {
     auto state = static_cast<struct android_app*>(m_State);
-    state->userData = this;
-
-    state->onAppCmd = [](struct android_app* state, int32_t cmd) {
-        auto app = static_cast<APP*>(state->userData);
-        switch(cmd) {
-            case APP_CMD_INIT_WINDOW:
-                // Potentially re-init renderer here if window was lost
-                break;
-            case APP_CMD_TERM_WINDOW:
-                break;
-            case APP_CMD_DESTROY:
-                app->m_Running = false;
-                break;
-        }
-    };
 
     while (m_Running) {
         int events;
         struct android_poll_source* source;
+        int timeout = (state->window == nullptr) ? -1 : 0; //  -1 (wait forever) if no window, 0 (don't wait) if rendering
 
-        while (ALooper_pollOnce(0, nullptr, &events, (void**)&source) >= 0) {
+        while (ALooper_pollOnce(timeout, nullptr, &events, (void**)&source) >= 0) {
             if (source) source->process(state, source);
             if (state->destroyRequested != 0) m_Running = false;
         }
 
-        if (m_Running) APP::loop_body(this);
+        if (m_Running && state->window != nullptr) APP::loop_body(this);
     }
 }
