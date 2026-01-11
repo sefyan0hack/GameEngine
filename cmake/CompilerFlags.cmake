f8b797f812aad005716fdb4c4e815b1cbf0ab5f3
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2026-01-11 13:55:54 +0100

    change order

--- cmake/CompilerFlags.cmake
+++ cmake/CompilerFlags.cmake
@@ -1,6 +1,6 @@
 
-set(MSVC_WARNING_FLAGS 
-    /W4 /permissive- /wd4251 /wd4275
+set(CLANG_AND_GNU_WARNING_FLAGS
+    -Wall -Wextra -Wpedantic
 )
 
 set(GNU_WARNING_FLAGS
@@ -11,14 +11,15 @@ set(CLANG_WARNING_FLAGS
     -Wno-language-extension-token
 )
 
-set(CLANG_AND_GNU_WARNING_FLAGS
-    -Wall -Wextra -Wpedantic
+set(MSVC_WARNING_FLAGS 
+    /W4 /permissive- /wd4251 /wd4275
 )
 
 add_library(project_warnings_flags INTERFACE)
 
 target_compile_options(project_warnings_flags INTERFACE
+    $<$<CXX_COMPILER_ID:Clang,GNU>:${CLANG_AND_GNU_WARNING_FLAGS}>
+    $<$<CXX_COMPILER_ID:GNU>:${GNU_WARNING_FLAGS}>
+    $<$<CXX_COMPILER_ID:Clang>:${CLANG_WARNING_FLAGS}>
     $<$<CXX_COMPILER_ID:MSVC>:${MSVC_WARNING_FLAGS}>
-    $<$<CXX_COMPILER_ID:GNU>:${CLANG_AND_GNU_WARNING_FLAGS};${GNU_WARNING_FLAGS}>
-    $<$<CXX_COMPILER_ID:Clang>:${CLANG_AND_GNU_WARNING_FLAGS};${CLANG_WARNING_FLAGS}>
 )
