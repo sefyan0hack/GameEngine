5afcf74e376aed23cfd8ca16d70613ef89393801
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2026-01-13 22:57:31 +0100

    +1 test case

--- tests/unit/Transform_test.cpp
+++ tests/unit/Transform_test.cpp
@@ -5,8 +5,15 @@ using namespace std;
 using namespace glm;
 
 TEST(Transform, ctor) {
-    auto r = Transform(vec3());
-    EXPECT_EQ(r.position, vec3());
-    EXPECT_EQ(r.rotation, vec3());
+    auto r = Transform(vec3(0.0f, 0.0f, 0.0f));
+    EXPECT_EQ(r.position, vec3(0.0f, 0.0f, 0.0f));
+    EXPECT_EQ(r.rotation, vec3(0.0f, 0.0f, 0.0f));
+    EXPECT_EQ(r.scale, vec3(1.0f, 1.0f, 1.0f));
+}
+
+TEST(Transform, empty_ctor) {
+    auto r = Transform();
+    EXPECT_EQ(r.position, vec3(0.0f, 0.0f, 0.0f));
+    EXPECT_EQ(r.rotation, vec3(0.0f, 0.0f, 0.0f));
     EXPECT_EQ(r.scale, vec3(1.0f, 1.0f, 1.0f));
 }
