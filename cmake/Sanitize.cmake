09357e969e7bcc921084eb26d9e2c1220f267f70
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2026-01-13 22:57:40 +0100

    fix sanitizer

--- cmake/Sanitize.cmake
+++ cmake/Sanitize.cmake
@@ -1,42 +1,36 @@
+set(CLANG_GNU_SANITIZER_FLAGS "")
 
 if(ASAN)
     if(MSVC)
-        set(MSVC_SANITIZER_FLAGS /fsanitize=address)
+        set(MSVC_SANITIZER_FLAGS "/fsanitize=address")
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-        set(CLANG_GNU_SANITIZER_FLAGS -fsanitize=address)
+        list(APPEND CLANG_GNU_SANITIZER_FLAGS "-fsanitize=address")
     endif()
-    
 endif()
 
 if(USAN)
     if(MSVC)
-        message(STATUS "USAN NOT WORKING IN `MSVC` !!")
+        message(STATUS "UBSan NOT supported in MSVC (AddressSanitizer only)")
     elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-        set(CLANG_GNU_SANITIZER_FLAGS -fsanitize=undefined)
+        list(APPEND CLANG_GNU_SANITIZER_FLAGS "-fsanitize=undefined")
     endif()
 endif()
 
-if(TSAN)
-    if(MSVC)
-        message(STATUS "TSAN NOT WORKING IN `MSVC` !!")
-    elseif(CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
-        set(CLANG_GNU_SANITIZER_FLAGS -fsanitize=thread)
-    endif()
-endif()
 
 add_library(project_sanitizer_flags INTERFACE)
 
 target_compile_options(project_sanitizer_flags INTERFACE
     $<$<CXX_COMPILER_ID:Clang,GNU>:${CLANG_GNU_SANITIZER_FLAGS}>
+    $<$<CXX_COMPILER_ID:MSVC>:${MSVC_SANITIZER_FLAGS}>
 )
 
 target_link_options(project_sanitizer_flags INTERFACE
     $<$<CXX_COMPILER_ID:Clang,GNU>:${CLANG_GNU_SANITIZER_FLAGS}>
+    $<$<CXX_COMPILER_ID:MSVC>:${MSVC_SANITIZER_FLAGS}>
 )
 
-if(ASAN OR USAN OR TSAN AND NOT WIN32 )
+if((ASAN OR USAN OR TSAN) AND NOT WIN32)
     target_link_options(project_sanitizer_flags INTERFACE
-        $<$<CXX_COMPILER_ID:GNU>:-static-libasan;-static-libubsan;-static-liblsan>
-        $<$<CXX_COMPILER_ID:Clang>:-static-libsan>
+        $<$<CXX_COMPILER_ID:GNU>:-static-libasan;-static-libubsan>
     )
 endif()
