989e0bc5f6069f0608d154fd955dd31536e58e7b
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2026-01-15 10:06:33 +0100

    some renaming

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -11,7 +11,7 @@ option(GAME_HOT_RELOADABLE  "Build Game as Shared and hot reload it"  OFF)
 option(RTTI                 "RunTime Type Info"                       ON)
 cmake_dependent_option(STATIC_LINK "Static link C++" ON "MINGW" OFF)
 cmake_dependent_option(ROBUST_GL_CHECK "Robust GL checks in Debug builds `slow` " ON [[ CMAKE_BUILD_TYPE STREQUAL "Debug" ]] OFF)
-cmake_dependent_option(NO_CONSOLE "No Console with Window" ON [[ CMAKE_BUILD_TYPE STREQUAL "Release" ]] OFF)
+cmake_dependent_option(CONSOLE_ATTACHED "Console Attached to Window" OFF [[ CMAKE_BUILD_TYPE STREQUAL "Release" ]] ON)
 
 
 ####################################################################################################
@@ -20,14 +20,14 @@ cmake_dependent_option(NO_CONSOLE "No Console with Window" ON [[ CMAKE_BUILD_TYP
 
 add_feature_info(TESTS                  TESTS                "[Build the testing suite]")
 add_feature_info(DOCS                   DOCS                 "[Build documentation]")
-add_feature_info(COVERAGE               COVERAGE      "[Generate coverage reports]")
+add_feature_info(COVERAGE               COVERAGE             "[Generate coverage reports]")
 add_feature_info(HARDEN                 HARDEN               "[Security hardening flags]")
 add_feature_info(ASAN                   ASAN                 "[Address Sanitizer]")
 add_feature_info(USAN                   USAN                 "[Undefined Sanitizer]")
 add_feature_info(LINK_TIME_OPT          LINK_TIME_OPT        "[Link Time optimization]")
 add_feature_info(GAME_HOT_RELOADABLE    GAME_HOT_RELOADABLE  "[Game hot reload support]")
 add_feature_info(ROBUST_GL_CHECK        ROBUST_GL_CHECK      "[Robust GL checks in Debug]")
-add_feature_info(NO_CONSOLE             NO_CONSOLE           "[No Console with Window]")
+add_feature_info(CONSOLE_ATTACHED       CONSOLE_ATTACHED     "[Console Attached  to Window]")
 add_feature_info(RTTI                   RTTI                 "[Run Time type information]")
 add_feature_info(STATIC_LINK            STATIC_LINK          "[Static link C++]")
 
