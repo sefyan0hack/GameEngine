569761aed62fd34e4b3b95a160f45f1c6882cf14
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2026-01-16 18:50:55 +0100

    cmake options for  extensive pringting

--- Engine/core/Log.hpp
+++ Engine/core/Log.hpp
@@ -27,5 +27,10 @@ namespace debug {
 
 } // namespace debug
 
-#define CTOR_LOG debug::log("({:p})  {} {}", (const void*)this, ::type_name<std::remove_pointer_t<decltype(this)>>(), *this);
-#define DTOR_LOG debug::log("({:p}) ~{}", (const void*)this, ::type_name<std::remove_pointer_t<decltype(this)>>());
+#if defined(OBJ_LIFETIME_TRACE)
+#   define CTOR_LOG debug::log("({:p})  {} {}", (const void*)this, ::type_name<std::remove_pointer_t<decltype(this)>>(), *this);
+#   define DTOR_LOG debug::log("({:p}) ~{}", (const void*)this, ::type_name<std::remove_pointer_t<decltype(this)>>());
+#else
+#   define CTOR_LOG
+#   define DTOR_LOG
+#endif
--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -3,16 +3,16 @@ include(CMakeDependentOption)
 option(TESTS                "Tests"                                   OFF)
 option(DOCS                 "Docs"                                    OFF)
 option(COVERAGE             "Code coverage"                           OFF)
-option(HARDEN               "Hardening"                               OFF)
+option(HARDEN               "Hardening"                               ON)
 option(ASAN                 "Address Sanitizer"                       OFF)
 option(USAN                 "Undefined Sanitizer"                     OFF)
 option(LINK_TIME_OPT        "Link Time opt"                           ON)
 option(GAME_HOT_RELOADABLE  "Build Game as Shared and hot reload it"  OFF)
 option(RTTI                 "RunTime Type Info"                       ON)
+option(OBJ_LIFETIME_TRACE   "Object life time tracing"                ON)
 cmake_dependent_option(STATIC_LINK "Static link C++" ON "MINGW" OFF)
 cmake_dependent_option(ROBUST_GL_CHECK "Robust GL checks in Debug builds `slow` " ON [[ CMAKE_BUILD_TYPE STREQUAL "Debug" ]] OFF)
 cmake_dependent_option(CONSOLE_ATTACHED "Console Attached to Window" OFF [[ CMAKE_BUILD_TYPE STREQUAL "Release" ]] ON)
-
 
 ####################################################################################################
 # Print Option Status
@@ -29,6 +29,7 @@ add_feature_info(GAME_HOT_RELOADABLE    GAME_HOT_RELOADABLE  "[Game hot reload s
 add_feature_info(ROBUST_GL_CHECK        ROBUST_GL_CHECK      "[Robust GL checks in Debug]")
 add_feature_info(CONSOLE_ATTACHED       CONSOLE_ATTACHED     "[Console Attached  to Window]")
 add_feature_info(RTTI                   RTTI                 "[Run Time type information]")
+add_feature_info(OBJ_LIFETIME_TRACE     OBJ_LIFETIME_TRACE   "[Object life time tracing]")
 add_feature_info(STATIC_LINK            STATIC_LINK          "[Static link C++]")
 
 feature_summary(WHAT ALL FATAL_ON_MISSING_REQUIRED_PACKAGES)
