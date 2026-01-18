5b81e7768042256cba25f590401737a422433e7b
sofyane0bentaleb <sofyane.bentaleb@hotmail.com>
2026-01-18 18:01:49 +0100

    fix coverage problem [skip CI]

--- cmake/Coverage.cmake
+++ cmake/Coverage.cmake
@@ -1,10 +1,11 @@
 if(COVERAGE)
-
     if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-        set( CLANG_COVERAGE_FLAGS
-            -fprofile-instr-generate
-            -fcoverage-mapping
-            -fcoverage-mcdc
+        # GCC Flags
+        set(GNU_COVERAGE_FLAGS
+            --coverage
+            -fno-inline
+            -fno-inline-small-functions
+            -fno-default-inline
         )
 
         find_program(GCOVR_PATH gcovr REQUIRED)
@@ -35,12 +36,11 @@ if(COVERAGE)
             )
 
             add_custom_target(pre_coverage
-                COMMAND ${GCOVR_PATH} --version
-                COMMAND ${GCOV_PATH} --version
-                COMMAND ${CMAKE_COMMAND} -E env "TESTING_ENABLED=1"  $<TARGET_FILE:unit_tests>
+                COMMAND ${CMAKE_COMMAND} $<TARGET_FILE:unit_tests>
                 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                 DEPENDS Engine unit_tests
             )
+
             add_custom_target(coverage
                 COMMAND ${GCOVR_PATH} ${COMMEN_GCOVR_FLAGS} --xml -o coverage
                 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
@@ -55,23 +55,21 @@ if(COVERAGE)
                 COMMENT "Generating GCC html coverage report with gcovr..."
             )
         endif()
-    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-        set( GNU_COVERAGE_FLAGS
-            --coverage
-            -fno-omit-frame-pointer
-            -fno-elide-constructors # Track copy/move constructors
-            -fprofile-abs-path # Absolute paths for gcovr
+
+    elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+        # Clang Flags (Source-based coverage)
+        set(CLANG_COVERAGE_FLAGS
+            -fprofile-instr-generate
+            -fcoverage-mapping
+            -fcoverage-mcdc
             -fno-inline
         )
 
-        # find_program(LCOV_PATH lcov REQUIRED)
         find_program(LLVM_PROFDATA_PATH llvm-profdata REQUIRED)
         find_program(LLVM_COV_PATH llvm-cov REQUIRED)
 
         add_custom_target(pre_coverage
-            COMMAND ${LLVM_COV_PATH} --version
-            COMMAND ${CMAKE_COMMAND} -E remove "default.profraw"
-            COMMAND ${CMAKE_COMMAND} -E remove "coverage.profdata"
+            COMMAND ${CMAKE_COMMAND} -E remove -f "default.profraw" "coverage.profdata"
             COMMAND ${CMAKE_COMMAND} -E env "LLVM_PROFILE_FILE=default.profraw" "TESTING_ENABLED=1" $<TARGET_FILE:unit_tests>
             COMMAND ${LLVM_PROFDATA_PATH} merge -sparse default.profraw -o coverage.profdata
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
@@ -82,39 +80,22 @@ if(COVERAGE)
             COMMAND ${LLVM_COV_PATH} export $<TARGET_FILE:unit_tests>
                 -instr-profile=coverage.profdata
                 -format=lcov
-                -ignore-filename-regex='${CMAKE_SOURCE_DIR}/tests/.*'
-                -ignore-filename-regex='${CMAKE_SOURCE_DIR}/3party/.*'
-                > ${CMAKE_BINARY_DIR}/coverage
+                -ignore-filename-regex='(tests/|3party/)'
+                > ${CMAKE_BINARY_DIR}/coverage.lcov
             WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
             DEPENDS pre_coverage
-            COMMENT "Generating lcov coverage report for Codecov.io"
-        )
-
-        add_custom_target(coverage_html
-            COMMAND ${LLVM_COV_PATH} show $<TARGET_FILE:unit_tests>
-                -instr-profile=coverage.profdata
-                -output-dir=${CMAKE_BINARY_DIR}/coverage_html
-                -format=html
-                -Xdemangler=c++filt -Xdemangler=-n
-                -show-line-counts-or-regions
-                -show-instantiations
-                -ignore-filename-regex='${CMAKE_SOURCE_DIR}/tests/.*'
-                -ignore-filename-regex='${CMAKE_SOURCE_DIR}/3party/.*'
-                --show-mcdc-summary
-                --show-mcdc
-                -show-instantiation-summary
-            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
-            DEPENDS pre_coverage
-            COMMENT "Generating Clang html coverage reports..."
         )
     endif()
-
 endif()
-
 
 add_library(project_coverage_flags INTERFACE)
 
 target_compile_options(project_coverage_flags INTERFACE
     $<$<CXX_COMPILER_ID:GNU>:${GNU_COVERAGE_FLAGS}>
     $<$<CXX_COMPILER_ID:Clang>:${CLANG_COVERAGE_FLAGS}>
+)
+
+target_link_options(project_coverage_flags INTERFACE
+    $<$<CXX_COMPILER_ID:GNU>:--coverage>
+    $<$<CXX_COMPILER_ID:Clang>:-fprofile-instr-generate>
 )
