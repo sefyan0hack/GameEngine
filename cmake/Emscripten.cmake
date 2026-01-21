233b7825b601425c68cd356ee01f5e0f9241af7e
sefyan0hack <sefyanhack@gmail.com>
2026-01-21 08:49:19 +0100

    Release bedploy [skip CI]

--- cmake/Emscripten.cmake
+++ cmake/Emscripten.cmake
@@ -10,14 +10,13 @@ if(EMSCRIPTEN)
 
     add_link_options(
         -sUSE_WEBGL2=1
+        -sFULL_ES3=1
         -sMIN_WEBGL_VERSION=1
         -sMAX_WEBGL_VERSION=2
-        -sALLOW_MEMORY_GROWTH=1 # -pthread + ALLOW_MEMORY_GROWTH may run non-wasm code slowly
-        -sFULL_ES3=1
+        -sALLOW_MEMORY_GROWTH=1
         -sDISABLE_EXCEPTION_THROWING=0
         -sDISABLE_EXCEPTION_CATCHING=0
         -sFILESYSTEM=1 # needed for DynLib 
-        -sEXIT_RUNTIME=1
         -sGL_DEBUG=1
     )
 
