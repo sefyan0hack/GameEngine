cd01636d54c76830d596b57bf7e60822694db837
sefyan0hack <sefyanhack@gmail.com>
2026-01-21 02:54:20 +0100

    fix deploy web ci

--- cmake/Emscripten.cmake
+++ cmake/Emscripten.cmake
@@ -34,28 +34,4 @@ if(EMSCRIPTEN)
         -sERROR_ON_UNDEFINED_SYMBOLS=0
     )
 
-    configure_file(
-        ${CMAKE_SOURCE_DIR}/cmake/index_template.cmake.in
-        ${CMAKE_BINARY_DIR}/index_html.cmake
-        @ONLY
-    )
-
-    add_custom_command(
-        OUTPUT ${CMAKE_BINARY_DIR}/index.html
-        COMMAND ${CMAKE_COMMAND} -E copy ${CMAKE_BINARY_DIR}/index_html.cmake ${CMAKE_BINARY_DIR}/index.html
-        DEPENDS ${CMAKE_BINARY_DIR}/index_html.cmake Launcher
-        COMMENT "Generating index.html"
-    )
-    
-    add_custom_target(generate_index_html ALL
-        DEPENDS ${CMAKE_BINARY_DIR}/index.html
-    )
-
-    # COOP and COEP headers trick
-    file(DOWNLOAD
-        "https://raw.githubusercontent.com/gzuidhof/coi-serviceworker/master/coi-serviceworker.js"
-        "${CMAKE_BINARY_DIR}/coi-serviceworker.js"
-        SHOW_PROGRESS
-    )
-
 endif()
