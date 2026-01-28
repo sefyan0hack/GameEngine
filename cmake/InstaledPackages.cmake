901ae1a874d2a0c9c703f465e12c607c1bb256c6
sefyan0hack <sefyanhack@gmail.com>
2026-01-28 23:17:45 +0100

    rem `NOT EMSCRIPTEN`

--- cmake/InstaledPackages.cmake
+++ cmake/InstaledPackages.cmake
@@ -1,27 +1,25 @@
 
-if(NOT EMSCRIPTEN)
-    # Find OpenGL package
-    if(NOT ANDROID)
-        find_package(OpenGL QUIET)
-        set_package_properties(OpenGL PROPERTIES
-            URL "https://www.opengl.org"
-            DESCRIPTION "A cross-language, cross-platform application programming interface for rendering 2D and 3D vector graphics."
-            TYPE REQUIRED
-            PURPOSE "Crucial for the rendering engine to work."
-        )
-    endif()
-
-    # Find X11 package
-    if(LINUX)
-        find_package(X11 QUIET)
-        set_package_properties(XCB PROPERTIES
-            DESCRIPTION "X11 Linux windowing lib"
-            URL "https://x.org"
-            TYPE REQUIRED
-            PURPOSE "Required to  to open windows in linux"
-        )
-    endif()
-
-    # Extract the core OpenGL library (first entry in the list)
-    list(GET OPENGL_LIBRARIES 0 CORE_OPENGL_LIB)
+# Find OpenGL package
+if(NOT ANDROID)
+    find_package(OpenGL QUIET)
+    set_package_properties(OpenGL PROPERTIES
+        URL "https://www.opengl.org"
+        DESCRIPTION "A cross-language, cross-platform application programming interface for rendering 2D and 3D vector graphics."
+        TYPE REQUIRED
+        PURPOSE "Crucial for the rendering engine to work."
+    )
 endif()
+
+# Find X11 package
+if(LINUX)
+    find_package(X11 QUIET)
+    set_package_properties(XCB PROPERTIES
+        DESCRIPTION "X11 Linux windowing lib"
+        URL "https://x.org"
+        TYPE REQUIRED
+        PURPOSE "Required to  to open windows in linux"
+    )
+endif()
+
+# Extract the core OpenGL library (first entry in the list)
+list(GET OPENGL_LIBRARIES 0 CORE_OPENGL_LIB)
