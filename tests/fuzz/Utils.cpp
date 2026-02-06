397cbc85e34d17fa4e9a14f39fe0a40fbee48ba3
sefyan0hack <sefyanhack@gmail.com>
2026-02-06 08:46:26 +0100

    fix conversion from 'char' to non-scalar type [skip CI]

--- tests/fuzz/Utils.cpp
+++ tests/fuzz/Utils.cpp
@@ -6,7 +6,7 @@
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
     const char* string = (const char*)Data;
-    std::string delim = (char)(std::rand() % 127);
+    std::string delim = {(char)(std::rand() % 127)};
 
     std::println("sting: {}", string);
     std::println("delim: {}", delim);
