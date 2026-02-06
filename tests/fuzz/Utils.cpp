9d8ac3371e385d4a5022a51e8ac4e570c5cfd83e
sefyan0hack <sefyanhack@gmail.com>
2026-02-06 09:12:48 +0100

    fix some bugs + `--output-on-failure` [skip CI]

--- tests/fuzz/Utils.cpp
+++ tests/fuzz/Utils.cpp
@@ -4,8 +4,14 @@
 #include <print>
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+    if (Size == 0)
+        return 0;
 
-    const char* string = (const char*)Data;
+    std::string string{
+        reinterpret_cast<const char*>(Data),
+        Size
+    };
+
     std::string delim = {(char)(std::rand() % 127)};
 
     std::println("sting: {}", string);
