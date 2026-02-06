1be08c4fa4df256253a5819780c4397ea85b9f42
sefyan0hack <sefyanhack@gmail.com>
2026-02-06 05:06:54 +0100

    indentation

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -1,21 +1,21 @@
 include(CMakeDependentOption)
 
 option(WITH_TESTS           "Tests"                                   OFF)
-option(WITH_DOCS                 "Docs"                                    OFF)
+option(WITH_DOCS            "Docs"                                    OFF)
 option(COVERAGE             "Code coverage"                           OFF)
 option(HARDEN               "Hardening"                               ON)
 option(LINK_TIME_OPT        "Link Time opt"                           ON)
 option(GAME_HOT_RELOADABLE  "Build Game as Shared and hot reload it"  OFF)
 option(OBJ_LIFETIME_TRACE   "Object life time tracing"                ON)
 option(ROBUST_GL_CHECK      "Robust GL checks in Debug builds `slow`" OFF)
-cmake_dependent_option(CONSOLE_ATTACHED "Console Attached to Window" OFF [[ CMAKE_BUILD_TYPE STREQUAL "Release" ]] ON)
+cmake_dependent_option(CONSOLE_ATTACHED "Console Attached to Window"  OFF [[ CMAKE_BUILD_TYPE STREQUAL "Release" ]] ON)
 
 ####################################################################################################
 # Print Option Status
 ####################################################################################################
 
 add_feature_info(WITH_TESTS             WITH_TESTS           "[Build the testing suite]")
-add_feature_info(WITH_DOCS              WITH_DOCS            "[Build documentation]")
+add_feature_info(WITH_DOCS              WITH_DOCS            "[Build Documentation]")
 add_feature_info(COVERAGE               COVERAGE             "[Generate coverage reports]")
 add_feature_info(HARDEN                 HARDEN               "[Security hardening flags]")
 add_feature_info(LINK_TIME_OPT          LINK_TIME_OPT        "[Link Time optimization]")
