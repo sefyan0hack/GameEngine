0a5713e4380b14cc45876a61b8a5c002e5da3acf
sefyan0hack <sefyanhack@gmail.com>
2026-02-10 19:37:46 +0100

    fix gl context cresation on web

--- Engine/graphics/platform/web/OpenGL.cpp
+++ Engine/graphics/platform/web/OpenGL.cpp
@@ -1,7 +1,5 @@
 #include "OpenGL.hpp"
 #include "Window.hpp"
-#include <core/Log.hpp>
-#include <cassert>
 
 OpenGL::~OpenGL()
 {
@@ -18,25 +16,22 @@ auto OpenGL::make_current_opengl()  -> bool
 auto OpenGL::create_opengl_context() -> GL_CTX
 {
     auto surface = m_Window.surface();
+
     EmscriptenWebGLContextAttributes attrs;
-    for(int depth = 0; depth <= 1; ++depth)
-    for(int stencil = 0; stencil <= 1; ++stencil)
-    for(int antialias = 0; antialias <= 1; ++antialias)
-    {
-        emscripten_webgl_init_context_attributes(&attrs);
+    emscripten_webgl_init_context_attributes(&attrs);
 
-        attrs.depth = depth;
-        attrs.stencil = stencil;
-        attrs.antialias = antialias;
+    attrs.majorVersion = 2;
+    attrs.minorVersion = 0;
 
-        debug::log("Requesting depth:{}, stencil: {}, antialias: {}", depth, stencil, antialias);
+    attrs.depth = 1;
+    attrs.stencil = 1;
+    attrs.antialias = 1;
 
-        auto context = emscripten_webgl_create_context(surface, &attrs);
+    auto context = emscripten_webgl_create_context(surface, &attrs);
 
-        if (context <= 0) {
-            throw Exception("Failed to create WebGL context: GPU acceleration may be disabled.");
-        }
-
-        return context;
+    if (context <= 0) {
+        throw Exception("Failed to create WebGL context: GPU acceleration may be disabled.");
     }
+
+    return context;
 }
