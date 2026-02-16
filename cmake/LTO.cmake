796b83966a1d77aa2236c2057e748927d867a4d4
sefyan0hack <sefyanhack@gmail.com>
2026-02-16 11:58:12 +0100

    rem `Ensure IPO/LTO policy is modern`

--- cmake/LTO.cmake
+++ cmake/LTO.cmake
@@ -4,8 +4,6 @@ if(LTO)
         message(STATUS "LTO with Coverage not allowed")
         return()
     endif()
-    # Ensure IPO/LTO policy is modern
-    cmake_policy(SET CMP0069 NEW)
 
     include(CheckIPOSupported)
     check_ipo_supported(RESULT IPO_SUPPORTED OUTPUT OUTPUT LANGUAGES CXX)
