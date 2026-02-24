2f792c3972888f85fa8e840686177c6a30db3545
sefyan0hack <sefyanhack@gmail.com>
2026-02-24 19:51:16 +0000

    check `GTEST_HAS_DEATH_TEST` [skip windows][skip linux]

--- tests/unit/Log.cpp
+++ tests/unit/Log.cpp
@@ -8,11 +8,18 @@ TEST(Log, LoggerShouldEqDefaultLogger)
 
 TEST(Log, UnimplimentedShouldabort)
 {
-  ASSERT_DEATH(debug::unimpl(), "");
+  #if defined(GTEST_HAS_DEATH_TEST) && GTEST_HAS_DEATH_TEST
+      ASSERT_DEATH(debug::unimpl(), ".*");
+  #else
+      GTEST_SKIP_("Death tests not supported on this platform (Emscripten)");
+  #endif
 }
 
 TEST(Log, UnrechableShouldabort)
 {
-  
-  ASSERT_DEATH(debug::unrechable(), "");
+  #if defined(GTEST_HAS_DEATH_TEST) && GTEST_HAS_DEATH_TEST
+      ASSERT_DEATH(debug::unrechable(), ".*");
+  #else
+      GTEST_SKIP_("Death tests not supported on this platform (Emscripten)");
+  #endif
 }
