f88db6ab999330791e0f990e1971eaf1d42a4728
sefyan0hack <sefyanhack@gmail.com>
2026-06-04 15:49:26 +0100

    use `CMAKE_SOURCE_DIR`

--- Engine/core/res.cpp
+++ Engine/core/res.cpp
@@ -8,9 +8,6 @@ static auto embed_filesystem = cmrc::res::get_filesystem();
 
 auto res::get(const char* res_name) -> std::span<const char>
 {
-    if (!embed_filesystem.exists(res_name))
-        throw Exception("Embedded resource not found: {}", res_name);
-
     auto f = embed_filesystem.open(res_name);
     return std::span<const char>(f.begin(), f.end());
 }
