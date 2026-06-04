5b2a63e0bf88e584d5fd19136bf864dee628ca55
sefyan0hack <sefyanhack@gmail.com>
2026-06-04 15:27:32 +0100

    check ifembed_filesystem file exists

--- Engine/core/res.cpp
+++ Engine/core/res.cpp
@@ -1,4 +1,5 @@
 #include "res.hpp"
+#include "Exception.hpp"
 #include <cmrc/cmrc.hpp>
 #include <engine_export.h>
 
@@ -7,6 +8,9 @@ static auto embed_filesystem = cmrc::res::get_filesystem();
 
 auto res::get(const char* res_name) -> std::span<const char>
 {
+    if (!embed_filesystem.exists(res_name))
+        throw Exception("Embedded resource not found: {}", res_name);
+
     auto f = embed_filesystem.open(res_name);
     return std::span<const char>(f.begin(), f.end());
 }
