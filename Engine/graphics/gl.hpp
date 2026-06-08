16101d61b1e468955c893e5d9c190514672de2f5
sefyan0hack <sefyanhack@gmail.com>
2026-06-08 18:57:34 +0100

    remove DrawArraysIndirect  not supported by `GL ES`

--- Engine/graphics/gl.hpp
+++ Engine/graphics/gl.hpp
@@ -116,7 +116,6 @@ namespace gl {
     inline decltype(&glUniformBlockBinding) UniformBlockBinding;
     inline decltype(&glGetBooleanv) GetBooleanv;
     inline decltype(&glFinish) Finish;
-    inline decltype(&glDrawArraysIndirect) DrawArraysIndirect;
 
     ENGINE_EXPORT auto get_proc_address(const char* name) -> void*;
     ENGINE_EXPORT auto load_opengl_functions() -> void;
