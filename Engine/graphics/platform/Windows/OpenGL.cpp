68f4be2e1d5937dd3883d59e5a19d29ab6d3cdc9
sefyan0hack <sefyanhack@gmail.com>
2026-06-27 09:06:54 +0100

    //TODO: make it platform egnostic

--- Engine/graphics/platform/Windows/OpenGL.cpp
+++ Engine/graphics/platform/Windows/OpenGL.cpp
@@ -96,9 +96,12 @@ auto OpenGL::create_context() -> GL_CTX
         }
     }
 
+    //TODO: make it platform egnostic
+    #ifdef _WIN32
     GET_GLEXT_FUNCTION_NO_THROW(wglGetExtensionsStringARB);
     if(wglGetExtensionsStringARB_ext)
         extensions += wglGetExtensionsStringARB_ext(dummy_surface);
+    #endif
 
     ////////////////////////////////////////////////////////
 
