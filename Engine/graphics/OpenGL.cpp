f47a54f02e9601567ec3228b26b5a040ee5af2fe
sefyan0hack <sefyanhack@gmail.com>
2026-07-06 18:20:56 +0100

    print more gl info

--- Engine/graphics/OpenGL.cpp
+++ Engine/graphics/OpenGL.cpp
@@ -76,6 +76,8 @@ OpenGL::OpenGL([[maybe_unused]] const CWindow& window)
     gl_info(GL_MAX_TEXTURE_BUFFER_SIZE);
     gl_info(GL_MAX_VERTEX_UNIFORM_COMPONENTS);
     gl_info(GL_MAX_FRAGMENT_UNIFORM_COMPONENTS);
+    gl_info(GL_MAX_UNIFORM_BLOCK_SIZE);
+    gl_info(GL_MAX_UNIFORM_BUFFER_BINDINGS);
     gl_info(GL_MAX_RENDERBUFFER_SIZE);
     gl_info(GL_SAMPLES);
     gl_info(GL_MAX_SAMPLES);
