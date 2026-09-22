b0a74f6c9c49d8c365b0b942697020b5329375f5
sefyan0hack <sefyanhack@gmail.com>
2026-09-22 19:51:03 +0100

    fix type

--- Engine/graphics/OpenGL.cpp
+++ Engine/graphics/OpenGL.cpp
@@ -568,7 +568,7 @@ auto gl::query_timestamp(GLuint id) -> void
     glQueryCounter_ext(id, GL_TIMESTAMP);
     #else
     if(OpenGL::is_GL_EXT_disjoint_timer_query){
-        GET_GLEXT_FUNCTION_THROW(QueryCounterEXT);
+        GET_GLEXT_FUNCTION_THROW(glQueryCounterEXT);
         QueryCounterEXT_ext(id, GL_TIMESTAMP_EXT);
     }
     #endif
